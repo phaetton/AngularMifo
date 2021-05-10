c7626ed2df83f1d58eb11ed0a3289a0a3e6a5ccf
phaetton <31552767+phaetton@users.noreply.github.com>
2021-05-10 12:03:01 -0600

    Ver mentor
    mostramos un mentor ingresado desde la url

--- src/app/detalle/detalle.component.ts
+++ src/app/detalle/detalle.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { MentoresService } from '../mentores.service';
+import { Mentor } from '../models/mentor.model';
 
 @Component({
   selector: 'app-detalle',
@@ -7,13 +9,17 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./detalle.component.scss']
 })
 export class DetalleComponent implements OnInit {
+  mentor: Mentor;
 
-  constructor(private activatedroute:ActivatedRoute) { }
+  constructor(
+    private activatedroute: ActivatedRoute,
+    private mentoresservice: MentoresService
+  ) { }
 
-  ngOnInit(): void {
+   ngOnInit() {
     //obtenemos el id ingresado en la url
-    this.activatedroute.params.subscribe(params=>{
-      console.log(params.mentorId);
+    this.activatedroute.params.subscribe(async params => {
+      this.mentor = await this.mentoresservice.getById(parseInt(params.mentorId));
     })
   }
 
