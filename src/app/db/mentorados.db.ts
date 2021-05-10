22c296298f1a2832041c8a02b7c0f9bd39d4c821
phaetton <31552767+phaetton@users.noreply.github.com>
2021-05-10 09:54:49 -0600

    Inicializando el proyecto
    Inicializamos el proyecto con una base de datos básica

---
+++ src/app/db/mentorados.db.ts
@@ -0,0 +1,103 @@
+import { Mentorado } from "../models/mentorado.model";
+
+export const MENTORADOS: Mentorado[] = [
+    {
+        id: 1,
+        nombre: 'Maria',
+        apellido: 'Perez',
+        email: 'maria@yimeil.com',
+        imagen: './assets/img/9.jpg',
+        celular: 12345678,
+        whatsapp: 87654321,
+        mentor: 1
+    },
+    {
+        id: 2,
+        nombre: 'Mario',
+        apellido: 'Perez',
+        email: 'mario@yimeil.com',
+        imagen: './assets/img/10.jpg',
+        celular: 12345678,
+        whatsapp: 87654321,
+        mentor: 2
+    },
+    {
+        id: 3,
+        nombre: 'Karen',
+        apellido: 'Zuarez',
+        email: 'karen@yimeil.com',
+        imagen: './assets/img/11.jpg',
+        celular: 12345678,
+        whatsapp: 87654321,
+        mentor: 3
+    },
+    {
+        id: 4,
+        nombre: 'Napoleon',
+        apellido: 'Bonaparte',
+        email: 'napoleon@yimeil.com',
+        imagen: './assets/img/12.jpg',
+        celular: 12345678,
+        whatsapp: 87654321,
+        mentor: 1
+    },
+    {
+        id: 5,
+        nombre: 'Edgar',
+        apellido: 'Lopez',
+        email: 'edgar@yimeil.com',
+        imagen: './assets/img/13.jpg',
+        celular: 12345678,
+        whatsapp: 87654321,
+        mentor: 2
+    },
+    {
+        id: 6,
+        nombre: 'Pedro',
+        apellido: 'Elisondo',
+        email: 'pedro@yimeil.com',
+        imagen: './assets/img/14.jpg',
+        celular: 12345678,
+        whatsapp: 87654321,
+        mentor: 3
+    },
+    {
+        id: 7,
+        nombre: 'Carmen',
+        apellido: 'Lunares',
+        email: 'carmen@yimeil.com',
+        imagen: './assets/img/15.jpg',
+        celular: 12345678,
+        whatsapp: 87654321,
+        mentor: 2
+    },
+    {
+        id: 8,
+        nombre: 'Dario',
+        apellido: 'Benjamin',
+        email: 'dario@yimeil.com',
+        imagen: './assets/img/16.jpg',
+        celular: 12345678,
+        whatsapp: 87654321,
+        mentor: 1
+    }
+    , {
+        id: 9,
+        nombre: 'Rayan',
+        apellido: 'Dribal',
+        email: 'rayan@yimeil.com',
+        imagen: './assets/img/17.jpg',
+        celular: 12345678,
+        whatsapp: 87654321,
+        mentor: 3
+    }, {
+        id: 10,
+        nombre: 'Xochilt',
+        apellido: 'Moican',
+        email: 'xochilt@yimeil.com',
+        imagen: './assets/img/18.jpg',
+        celular: 12345678,
+        whatsapp: 87654321,
+        mentor: 1
+    }
+]
---
+++ src/app/db/mentores.db.ts
@@ -0,0 +1,84 @@
+import { Mentor } from "../models/mentor.model";
+
+export const MENTORES: Mentor[] = [
+    {
+        id: 1,
+        nombre: 'Jose',
+        apellido: 'Fonseca',
+        email: 'jose@mentorsinternational.org',
+        imagen: './assets/img/1.jpg',
+        celular: 12345678,
+        whatsapp: 87654321,
+        oficina: 'Managua'
+    },
+    {
+        id: 2,
+        nombre: 'Mario',
+        apellido: 'Aguirre',
+        email: 'mario@mentorsinternational.org',
+        imagen: './assets/img/2.jpg',
+        celular: 12345678,
+        whatsapp: 87654321,
+        oficina: 'Merida'
+    },
+    {
+        id: 3,
+        nombre: 'Karen',
+        apellido: 'Baca',
+        email: 'karen@mentorsinternational.org',
+        imagen: './assets/img/3.jpg',
+        celular: 12345678,
+        whatsapp: 87654321,
+        oficina: 'Managua'
+    },
+    {
+        id: 4,
+        nombre: 'Napoleon',
+        apellido: 'Bryan',
+        email: 'napoleon@mentorsinternational.org',
+        imagen: './assets/img/4.jpg',
+        celular: 12345678,
+        whatsapp: 87654321,
+        oficina: 'Honduras'
+    },
+    {
+        id: 5,
+        nombre: 'ChanChan',
+        apellido: 'Chann',
+        email: 'chanchan@mentorsinternational.org',
+        imagen: './assets/img/5.jpg',
+        celular: 12345678,
+        whatsapp: 87654321,
+        oficina: 'Managua'
+    },
+    {
+        id: 6,
+        nombre: 'Pedro',
+        apellido: 'Perez',
+        email: 'pedro@mentorsinternational.org',
+        imagen: './assets/img/6.jpg',
+        celular: 12345678,
+        whatsapp: 87654321,
+        oficina: 'Honduras'
+    },
+    {
+        id: 7,
+        nombre: 'Carmen',
+        apellido: 'Centeno',
+        email: 'carmen@mentorsinternational.org',
+        imagen: './assets/img/7.jpg',
+        celular: 12345678,
+        whatsapp: 87654321,
+        oficina: 'Managua'
+    },
+    {
+        id: 8,
+        nombre: 'Dario',
+        apellido: 'Danes',
+        email: 'dario@mentorsinternational.org',
+        imagen: './assets/img/8.jpg',
+        celular: 12345678,
+        whatsapp: 87654321,
+        oficina: 'Managua'
+    }
+]
