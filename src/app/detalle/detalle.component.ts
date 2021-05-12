2b5ef67c9c285d37b1d6c808a3a6ed19573a9e52
phaetton <31552767+phaetton@users.noreply.github.com>
2021-05-12 11:46:35 -0600

    boton cambio de estado
    cambia vista mentor a mentorado y viceversa

--- src/app/detalle/detalle.component.ts
+++ src/app/detalle/detalle.component.ts
@@ -10,16 +10,36 @@ import { Mentor } from '../models/mentor.model';
 })
 export class DetalleComponent implements OnInit {
   mentor: Mentor;
+  vermentor:boolean ;
+  vermentorado:boolean ;
+  textobotonmentor:string;
 
   constructor(
     private activatedroute: ActivatedRoute,
     private mentoresservice: MentoresService
-  ) { }
+  ) {
+
+    this.vermentor=true;
+    this.vermentorado=true;
+
+  }
+
 
    ngOnInit() {
     //obtenemos el id ingresado en la url
     this.activatedroute.params.subscribe(async params => {
       this.mentor = await this.mentoresservice.getById(parseInt(params.mentorId));
+      this.vermentor=false;
+      this.vermentorado=true;
+      this.textobotonmentor="Perfil Mentor";
     })
+  }
+
+  onclick(){
+    this.vermentorado=!this.vermentorado;
+    this.vermentor=!this.vermentor;
+
+    this.textobotonmentor= this.vermentorado ?"Perfil Mentor":"Mentorados";
+
   }
 }
